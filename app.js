d44de617e9ecfb23c2c6e61e4330daee4b9077e7
Ryan Barley <rmbarley@gmail.com>
2016-03-15 22:55:56 -0400

    Set up mongodb for heroku

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
   campgroundRoutes = require("./routes/campgrounds"),
   indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://ryan:pizzadog@ds015859.mlab.com:15859/yelpcamp_rmb");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
